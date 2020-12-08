c997a3b9ed1cef9558a246f3ae6f4e82d32e9fcc
Lucas César <lucasambrogi1@hotmail.com>
2020-12-07 23:41:17 -0300

    Router and users module: created.

---
+++ src/shared/infra/http/routes/index.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+import usersRouter from '@modules/users/infra/http/routes/users.routes';
+
+const router = Router();
+
+router.use('users', usersRouter);
+
+export default router;
