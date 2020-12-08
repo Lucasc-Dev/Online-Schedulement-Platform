21444972188ec9823e8b69ab2f44d881a56ca2f2
Lucas César <lucasambrogi1@hotmail.com>
2020-12-07 23:43:42 -0300

    First route and users controller: created.

---
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+export default class UsersController {
+    public async create(request: Request, response: Response): Promise<Response> {
+        
+
+        return response.json();
+    }
+}
