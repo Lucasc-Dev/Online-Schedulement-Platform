43142d91b3aecfe1b9c9ceeb0e644b150e17f172
Lucas César <lucasambrogi1@hotmail.com>
2020-12-09 20:47:24 -0300

    .env file: installed dotenv lib and created .env.example.

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
+import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 
 const router = Router();
 
 router.use('/users', usersRouter);
+router.use('/sessions', sessionsRouter);
 
 export default router;
