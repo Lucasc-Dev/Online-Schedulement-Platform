fa2107d3951b6a42abe02a5106f87eed9720ea98
Lucas César <lucasambrogi1@hotmail.com>
2020-12-11 20:07:28 -0300

    User entity: implemented relation with roles entity.

--- src/modules/users/infra/typeorm/entities/User.ts
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -1,4 +1,6 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, ManyToMany, JoinTable, JoinColumn } from 'typeorm';
+
+import Role from './Role';
 
 @Entity('users')
 export default class User {
@@ -13,6 +15,18 @@ export default class User {
 
     @Column()
     password: string;
+
+    @ManyToMany(type => Role)
+    @JoinTable({ 
+        name: 'users_roles',
+        joinColumn: {
+            name: 'user_id',
+        },
+        inverseJoinColumn: {
+            name: 'role_id',
+        },
+    })
+    roles: Role[];
 
     @CreateDateColumn()
     created_at: Date;
