2283e51de86c3499bc14275d64edfeaca28e2ef9
Lucas César <lucasambrogi1@hotmail.com>
2020-12-14 23:43:37 -0300

    tsyringe: moved repository registers from shared to users module.

--- src/modules/users/providers/index.ts
+++ src/modules/users/providers/index.ts
@@ -1,10 +1,35 @@
 import { container } from 'tsyringe';
 
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+
+import IRolesRepository from '@modules/users/repositories/IRolesRepository';
+import RolesRepository from '@modules/users/infra/typeorm/repositories/RolesRepository';
+
+import IPermissionsRepository from '@modules/users/repositories/IPermissionsRepository';
+import PermissionsRepository from '@modules/users/infra/typeorm/repositories/PermissionsRepository';
+
 import ITokenProvider from './TokenProvider/models/ITokenProvider';
 import JsonWebToken from './TokenProvider/implementations/JsonWebToken';
 
 import IHashProvider from './HashProvider/models/IHashProvider';
 import BCryptHashProvider from './HashProvider/implementations/BCryptHashProvider';
+
+
+container.registerSingleton<IUsersRepository>(
+    'UsersRepository',
+    UsersRepository,
+);
+
+container.registerSingleton<IRolesRepository>(
+    'RolesRepository',
+    RolesRepository,
+);
+
+container.registerSingleton<IPermissionsRepository>(
+    'PermissionsRepository',
+    PermissionsRepository,
+);
 
 container.registerSingleton<ITokenProvider>(
     'TokenProvider',
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,27 +1,3 @@
 import { container } from 'tsyringe';
 
 import '@modules/users/providers';
-
-import IUsersRepository from '@modules/users/repositories/IUsersRepository';
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
-
-import IRolesRepository from '@modules/users/repositories/IRolesRepository';
-import RolesRepository from '@modules/users/infra/typeorm/repositories/RolesRepository';
-
-import IPermissionsRepository from '@modules/users/repositories/IPermissionsRepository';
-import PermissionsRepository from '@modules/users/infra/typeorm/repositories/PermissionsRepository';
-
-container.registerSingleton<IUsersRepository>(
-    'UsersRepository',
-    UsersRepository,
-)
-
-container.registerSingleton<IRolesRepository>(
-    'RolesRepository',
-    RolesRepository,
-)
-
-container.registerSingleton<IPermissionsRepository>(
-    'PermissionsRepository',
-    PermissionsRepository,
-)
