086ce8b3ba69f764d4200d08e67763a3012eacac
Lucas César <lucasambrogi1@hotmail.com>
2020-12-15 23:18:37 -0300

    Users router and its controller: implemented delete route.

--- src/modules/users/infra/http/controllers/UsersController.ts
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -4,6 +4,7 @@ import { container } from 'tsyringe';
 import CreateUserService from "@modules/users/services/CreateUserService";
 import UpdateUserProfileService from "@modules/users/services/UpdateUserProfileService";
 import ShowUserProfileService from "@modules/users/services/ShowUserProfileService";
+import DeleteUserService from "@modules/users/services/DeleteUserService";
 
 export default class UsersController {
     public async show(request: Request, response: Response): Promise<Response> {
@@ -35,5 +36,15 @@ export default class UsersController {
         const user = await updateUserProfile.execute({ user_id, name, email, password });
 
         return response.json(user);
+    }
+
+    public async delete(request: Request, response: Response): Promise<Response> {
+        const { user_id } = request.params;
+
+        const deleteUser = container.resolve(DeleteUserService);
+
+        await deleteUser.execute({ user_id });
+
+        return response.status(204).send();
     }
 }
--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -9,6 +9,7 @@ const usersController = new UsersController();
 
 router.get('/:user_id', permission('USER_MANAGE'), usersController.show);
 router.put('/:user_id', permission('USER_MANAGE'), usersController.update);
+router.delete('/:user_id', permission('USER_MANAGE'), usersController.delete);
 router.post('', usersController.create);
 
 export default router;
