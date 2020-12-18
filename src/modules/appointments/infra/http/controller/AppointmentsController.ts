3816ad98ef7cce2e8ffbe7c114aa0e7454ce1f24
Lucas César <lucasambrogi1@hotmail.com>
2020-12-17 23:55:24 -0300

    Create appointment route: implemented create appointment service.

--- src/modules/appointments/infra/http/controller/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controller/AppointmentsController.ts
@@ -1,8 +1,18 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import CreateAppointmentService from "@modules/appointments/services/CreateAppointmentService";
 
 export default class appointmentsController {
     public async create(request: Request, response: Response): Promise<Response> {
+        const { reason, status, date, doctor_id, patient } = request.body;
+
+        const createAppointment = container.resolve(CreateAppointmentService)
+
+        const appointment = await createAppointment.execute({ 
+            reason, status, date, doctor_id, patient 
+        });
         
-        return response.json();
+        return response.json(appointment);
     }
 }
